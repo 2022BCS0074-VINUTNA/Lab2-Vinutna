1468f46504c2ba2479165456fd1994c483869dda
pranathi <mantri22bcs25@iiitkottayam.ac.in>
2026-01-09 16:04:54 +0530

    Model-RandomForest, Data Split Strategy

--- train.py
+++ train.py
@@ -16,23 +16,20 @@ os.makedirs("output", exist_ok=True)
 # Load dataset
 df = pd.read_csv("dataset/winequality-red.csv", sep=";")
 
-selected_features = [
-    "alcohol", "sulphates", "pH", "volatile acidity"
-]
-
-X_subset = df[selected_features]
+X = df.drop("quality", axis=1)
 y = df["quality"]
 
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(
-    X_subset, y, test_size=0.2, random_state=42
+    X, y, test_size=0.3, random_state=42
 )
 
-# Experiment 6: Random Forest Feature Subset Selection
+# Experiment 7: Random Forest Data Split Strategy
 pipeline = RandomForestRegressor(
     n_estimators=100,
     random_state=42
 )
+
 
 # Train model
 pipeline.fit(X_train, y_train)
