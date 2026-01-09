72373517c18f99dd0dff1406b58945c03c6bb2e4
2022BCS0074-VINUTNA <vinutna22bcs74@iiitkottayam.ac.in>
2026-01-10 00:31:04 +0530

    Update train.py

--- train.py
+++ train.py
@@ -21,14 +21,13 @@ y = df["quality"]
 
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42
+    X, y, test_size=0.2, random_state=42  # 80/20 split for EXP-01
 )
 
-# Experiment 7: Random Forest Data Split Strategy
-pipeline = RandomForestRegressor(
-    n_estimators=100,
-    random_state=42
-)
+
+# Experiment 1: Linear Regression Default
+pipeline = LinearRegression()
+
 
 
 # Train model
